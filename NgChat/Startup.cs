feff47eb0c7f628e941a1bd7d69667fab4fa98c9
Ha Hoang <hoangnsau@gmail.com>
2017-10-15 14:53:15 +0700

    update the source code

---
+++ NgChat/Startup.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.Extensions.Configuration;
+using NgChat.SignalR;
+
+namespace NgChat
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSignalR();
+
+            //Handle XSRF Name for Header
+            services.AddAntiforgery(options => {
+                options.HeaderName = "X-XSRF-TOKEN";
+            });
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) => {
+                await next();
+                if (context.Response.StatusCode == 404 &&
+                   !Path.HasExtension(context.Request.Path.Value) &&
+                   (!context.Request.Path.Value.StartsWith("/api/") || !context.Request.Path.Value.StartsWith("/chat")))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
+
+            //This would need to be locked down as needed (very open right now)
+            app.UseCors((corsPolicyBuilder) =>
+            {
+                corsPolicyBuilder.AllowAnyOrigin();
+                corsPolicyBuilder.AllowAnyMethod();
+                corsPolicyBuilder.AllowAnyHeader();
+            });
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("chat");
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseMvcWithDefaultRoute();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
---
+++ NgChat/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
+
+import { Observable } from 'rxjs/Observable';
+import { HubConnection } from '@aspnet/signalr-client';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+    private _hubConnection: HubConnection;
+    public async: any;
+    message = '';
+    messages: string[] = [];
+
+    public sendMessage(): void {
+        const data = `Sent: ${this.message}`;
+
+        this._hubConnection.invoke('Send', data);
+        this.messages.push(data);
+    }
+
+    ngOnInit() {
+        this._hubConnection = new HubConnection('/chat');
+
+        this._hubConnection.on('Send', (data: any) => {
+            const received = `Received: ${data}`;
+            this.messages.push(received);
+        });
+
+        this._hubConnection.start()
+            .then(() => {
+                console.log('Hub connection started')
+            })
+            .catch(err => {
+                console.log('Error while establishing connection')
+            });
+    }
+}
